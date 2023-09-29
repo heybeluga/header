e9e30a29a6c454fb3e56fd0ebd5fbf6ceda7bfa9
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-09-29 13:15:08 -0400

    copy header content from website repo

---
+++ src/header-content.ts
@@ -0,0 +1,81 @@
+import type { HeaderDropdown, NavDropdownColumnProps } from './types'
+
+const TOOLS_COLUMNS: NavDropdownColumnProps[] = [
+  {
+    title: 'Portfolio Tracker',
+    subtitle: 'Your home for your crypto',
+    groupHref: 'https://dashboard.heybeluga.com/',
+    svgName: 'chart-donut',
+    iconBackground: 'bg-blue-50',
+    hoverBackground: 'hover:bg-blue-50',
+    links: [],
+    hasHeader: true
+  },
+  {
+    title: 'Crypto Calendar',
+    subtitle: 'Upcoming crypto events',
+    groupHref: 'https://calendar.heybeluga.com/',
+    svgName: 'calendar-event',
+    iconBackground: 'bg-red-50',
+    hoverBackground: 'hover:bg-red-50',
+    links: [],
+    hasHeader: true
+  }
+]
+const COMPARE_COLUMNS: NavDropdownColumnProps[] = [
+  {
+    title: 'Crypto Companies',
+    subtitle: 'Crypto companies and their products',
+    svgName: 'wallet',
+    links: [
+      { href: '/comparisons/nft-marketplaces/', text: 'NFT Marketplaces' },
+      {
+        href: '/comparisons/centralized-crypto-staking-platforms/',
+        text: 'Staking Platforms'
+      },
+      { href: '/comparisons/crypto-stocks/', text: 'Public Stocks' }
+    ]
+  }
+]
+const LEARN_COLUMNS: NavDropdownColumnProps[] = [
+  {
+    title: 'Blockchains',
+    subtitle: 'The building blocks of crypto',
+    svgName: 'link',
+    links: [
+      { href: '/articles/bitcoin/', text: 'Bitcoin' },
+      { href: '/articles/ethereum/', text: 'Ethereum' },
+      { href: '/articles/litecoin/', text: 'Litecoin' },
+      { href: '/articles/dogecoin/', text: 'Dogecoin' },
+      { href: '/articles/solana/', text: 'Solana' },
+      { href: '/articles/cosmos/', text: 'Cosmos' }
+    ]
+  },
+  {
+    title: 'DeFi',
+    subtitle: 'Decentralized finance',
+    svgName: 'topology-star',
+    links: [
+      { href: '/articles/defi/', text: 'DeFi' },
+      { href: '/articles/uniswap/', text: 'Uniswap' },
+      { href: '/articles/lido/', text: 'Lido' },
+      { href: '/articles/aave/', text: 'Aave' },
+      { href: '/articles/dydx/', text: 'dYdX' }
+    ]
+  },
+  {
+    title: 'NFTs',
+    subtitle: 'Non-fungible tokens',
+    svgName: 'photo',
+    links: [
+      { href: '/articles/nfts/', text: 'Bored Ape Yacht Club' },
+      { href: '/articles/opensea/', text: 'OpenSea' }
+    ]
+  }
+]
+
+export const HEADER_DROPDOWNS: HeaderDropdown[] = [
+  { title: 'Tools', body: TOOLS_COLUMNS },
+  { title: 'Compare', body: COMPARE_COLUMNS },
+  { title: 'Learn', body: LEARN_COLUMNS }
+]
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,20 @@
+export interface HeaderDropdown {
+  title: string
+  body: NavDropdownColumnProps[]
+}
+
 export interface Link {
   href: string
   text: string
+}
+
+export interface NavDropdownColumnProps {
+  title: string
+  subtitle: string
+  groupHref?: string
+  links: Link[]
+  svgName: string
+  iconBackground?: string
+  hoverBackground?: string
+  hasHeader?: boolean
 }
