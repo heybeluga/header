1a6272e75060fc0ec23916e0bd0c6b949c9b0ad4
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-09-30 14:06:55 -0400

    add Logo component using logo.svg contents

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { h } from 'preact'
-
 import type { FunctionalComponent } from 'preact'
+import { Logo } from './Logo'
 
 export const Header: FunctionalComponent<Record<string, null>> = () => {
   return (
     <div>
+      <Logo />
       <h1>Header</h1>
     </div>
   )
--- src/render.tsx
+++ src/render.tsx
@@ -1,4 +1,4 @@
-import { h, render } from 'preact'
+import { render } from 'preact'
 import { Header } from './index'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
