44246754d6873ba70145462fcf7d23da40844e4d
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-10-02 11:34:23 -0400

    install Tailwind CSS

--- src/render.tsx
+++ src/render.tsx
@@ -1,3 +1,5 @@
+import './main.css'
+
 import { render } from 'preact'
 import { Header } from './index'
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
