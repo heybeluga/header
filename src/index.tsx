641089b5a0fa452904fc2c85ab297ef945527bda
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-10-02 17:16:03 -0400

    copy full Header component from website repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,99 @@
-import { Chevron } from './Chevron'
+import classNames from 'classnames'
+import { ButtonLink } from './ButtonLink'
 import { Logo } from './Logo'
 import { NavDropdown } from './NavDropdown'
 import { HEADER_DROPDOWNS } from './header-content'
+import { mainDivClassNames } from './utils'
 
 import type { FunctionalComponent } from 'preact'
+import { NavHamburger } from './NavHamburger'
 import type { HeaderDropdown } from './types'
 
-export const Header: FunctionalComponent<Record<string, null>> = () => {
+type Props = Record<string, null>
+
+export const Header: FunctionalComponent<Props> = () => {
   return (
-    <div>
-      <Logo />
-      <Chevron />
-      {HEADER_DROPDOWNS.map(({ title, body }: HeaderDropdown) => (
-        <NavDropdown label={title} columns={body} />
-      ))}
-      <h1>Header</h1>
-    </div>
+    <header class="not-prose bg-white">
+      <nav
+        class={classNames(
+          ...mainDivClassNames,
+          'flex',
+          'justify-between',
+          'items-center',
+          'py-4'
+        )}
+      >
+        <div
+          class={classNames(
+            'w-full',
+            'flex',
+            'flex-wrap',
+            'items-center',
+            'justify-between'
+          )}
+        >
+          <Logo />
+          <div class="flex justify-between items-center">
+            <div class="flex desktop:order-2 ml-6">
+              <div class="hidden tablet:block">
+                <ButtonLink
+                  href="https://beluga.beehiiv.com/subscribe"
+                  text="Subscribe"
+                />
+              </div>
+              <button
+                data-collapse-toggle="nav-hamburger"
+                type="button"
+                class={classNames(
+                  'inline-flex',
+                  'items-center',
+                  'p-2',
+                  'ml-3',
+                  'text-sm',
+                  'text-slate-500',
+                  'rounded-lg',
+                  'desktop:hidden',
+                  'hover:bg-slate-100'
+                )}
+              >
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div
+              class={classNames(
+                'hidden',
+                'w-full',
+                'desktop:flex',
+                'items-center',
+                'justify-between',
+                'desktop:w-auto',
+                'desktop:order-1'
+              )}
+            >
+              <ul class="flex items-center">
+                {HEADER_DROPDOWNS.map(({ title, body }: HeaderDropdown) => (
+                  <NavDropdown label={title} columns={body} />
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div id="nav-hamburger" class="hidden w-full desktop:hidden">
+            <NavHamburger />
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,18 @@ export const capitalize = (word: string): string =>
 
 export const appendTrailingSlash = (href: string): string =>
   href.endsWith('/') ? href : `${href}/`
+
+export const MAIN_WRAPPER_WIDTH_CLASSES: string[] = [
+  'px-4',
+  'tablet:px-8',
+  'desktop:px-20',
+  'full:px-0',
+  'full:mx-auto',
+  'full:w-[1280px]'
+]
+
+export const mainDivClassNames: string[] = [
+  'text-slate-800',
+  'marker:text-slate-800',
+  ...MAIN_WRAPPER_WIDTH_CLASSES
+]
