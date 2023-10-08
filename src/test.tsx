a73f98e2ae591172bd1eaac32c0f5bf83654758c
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-10-08 18:52:58 -0400

    replace test component with actual header

--- src/test.tsx
+++
@@ -1,3 +0,0 @@
-export const Header = () => {
-  return <header className="not-prose bg-white">Beluga Header</header>
-}
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/test.tsx'),
+      entry: resolve(__dirname, 'src/index.tsx'),
       name: 'header',
       fileName: 'header'
     }
