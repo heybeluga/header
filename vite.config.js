12bcbfadc830cdbc6a9df614a77ac4f932953d21
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-10-09 20:10:28 -0400

    install vite-plugin-dts to generate type definitions

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'header',
+      name: 'header'
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -16,5 +17,6 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
+  plugins: [dts()]
 })
