b2d9eb23112fb2859680ee0f05f424d0cec0da72
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2023-11-18 13:40:21 -0500

    downgrade to React 17 to fix mobile hamburger rendering bug

--- src/render.tsx
+++ src/render.tsx
@@ -1,14 +1,15 @@
 import './main.css'
+
 import { StrictMode } from 'react'
+import { render } from 'react-dom'
 import { Header } from './Header'
-import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById('app') as HTMLElement)
-
-root.render(
+// The createRoot().render() API breaks the mobile hamburger navigation
+render(
   <StrictMode>
     <Header showSubscribe={true} showThemeToggler={true}>
       <p>Hello, world!</p>
     </Header>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('app')
 )
